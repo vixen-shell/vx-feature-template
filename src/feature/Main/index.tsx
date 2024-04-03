5040e983fc170b559663d722991830ede74b7866
Nohavye <noha.poncelet@gmail.com>
2024-04-03 14:52:21 +0200

    Initial commit

---
+++ src/feature/Main/index.tsx
@@ -0,0 +1,9 @@
+import { ui } from 'vx-front'
+
+export default function Main() {
+    return (
+        <ui.Frame gap={64}>
+            <h1>Hello Feature !!!</h1>
+        </ui.Frame>
+    )
+}
---
+++ src/feature/index.tsx
@@ -0,0 +1,7 @@
+import { Feature } from 'vx-front'
+
+import Main from './Main'
+
+export default Feature.init({
+    main: <Main />,
+})
