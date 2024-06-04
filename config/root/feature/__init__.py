2db51eba3acaca63d990904a93ef723834260de0
Nohavye <noha.poncelet@gmail.com>
2024-06-04 10:33:51 +0200

    Update feature module

--- config/root/feature/__init__.py
+++ config/root/feature/__init__.py
@@ -1,7 +1,6 @@
 from vx_feature_utils import Utils
 
-utils = Utils()
-
+utils = Utils.define_feature_utils()
 content = utils.define_feature_content(
     {
         "frames": {
@@ -14,3 +13,16 @@ content = utils.define_feature_content(
         "state": "disable",
     }
 )
+
+
+@content.add_handler("data")
+def date():
+    from datetime import datetime
+
+    date = datetime.now()
+
+    return {
+        "day": date.strftime("%d"),
+        "mounth": date.strftime("%m"),
+        "year": date.strftime("%Y"),
+    }
