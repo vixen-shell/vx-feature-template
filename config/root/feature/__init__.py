c36a740e92e145ff08cadd042c9ff39739409d30
Nohavye <noha.poncelet@gmail.com>
2024-06-04 17:56:43 +0200

    Global update

--- config/root/feature/__init__.py
+++ config/root/feature/__init__.py
@@ -1,7 +1,7 @@
 from vx_feature_utils import Utils
 
 utils = Utils.define_feature_utils()
-content = utils.define_feature_content(
+content = Utils.define_feature_content(
     {
         "frames": {
             "main": {
--- src/feature/Main/index.tsx
+++ src/feature/Main/index.tsx
@@ -1,9 +1,18 @@
-import { ui } from 'vx-front'
+import { ui, Feature } from 'vx-front'
 
 export default function Main() {
     return (
         <ui.Frame gap={64}>
-            <h1>Hello Feature !!!</h1>
+            <h1>Feature '{Feature.featureName}'</h1>
+            <p>
+                You have just created a new feature without a front-end for
+                Vixen Shell.
+                <br />
+                All you have to do now is implement the different contents of
+                this feature.
+                <br />
+                Happy developing!
+            </p>
         </ui.Frame>
     )
 }
